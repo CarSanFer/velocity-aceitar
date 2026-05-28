f27b9758cf9eef68dd33647090e7803b677ae363
CarSanFer <96453719+CarSanFer@users.noreply.github.com>
2026-05-28 14:59:29 +0100

    Create schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+cd <pasta-com-os-3-ficheiros>
+git init
+git add index.html README.md db/schema.sql
+git commit -m "Velocity v26.05.27.1910 — initial commit"
+git branch -M main
+git remote add origin https://github.com/CarSanFer/velocity-aceitar.git
+git push -u origin main
